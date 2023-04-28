6427fe3447cca57ba9c7a73a4183683ffdfd789d
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2023-04-28 16:35:38 -0500

    Caso 3 - Terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,41 +8,50 @@ public class Main
 	{
 		//Se obtienen los datos por parte del usuario.
 		Scanner scanner = new Scanner(System.in);
+		int opcionAlgoritmo;
+		String algoritmoEncriptacion = "";
+		do{
+			System.out.println("Ingrese el nombre del Algoritmo de generación de Hash, 1 para SHA256 o 2 para SHA512: ");
+			opcionAlgoritmo = scanner.nextInt();
+		} while (opcionAlgoritmo != 1 && opcionAlgoritmo != 2);
 
-		System.out.println("Ingrese el nombre del Algoritmo de generación de Hash,1 para SHA256 o 2 para SHA512 : ");
-		int algoritmoEncriptacion = Integer.parseInt(scanner.nextLine());
+		scanner.nextLine();
+
+		if (opcionAlgoritmo == 1){
+			algoritmoEncriptacion = "SHA-256"; 
+		} else{
+			algoritmoEncriptacion = "SHA-512";
+		}
 		
 		System.out.println("Ingrese la cadena que representa el código Hash de la contraseña que buscamos: ");
 		String contraseñaHash = scanner.nextLine();
+		byte[] hashBytes = new byte[contraseñaHash.length() / 2];
+		for (int i = 0; i < contraseñaHash.length(); i += 2) {
+            int hexValue = Integer.parseInt(contraseñaHash.substring(i, i + 2), 16);
+            hashBytes[i / 2] = (byte) hexValue;
+        }
 
 		System.out.println("Ingrese la Sal: ");
 		String sal = scanner.nextLine();
 
-		int numeroThreads = 1;
-		try
-		{
+		int numeroThreads;
+		do{
 			System.out.println("Ingrese el numero de threads que se ejecutaran: ");
-			numeroThreads =Integer.parseInt(scanner.nextLine());
-		}
-		catch (Exception numero)
-		{
-			System.out.println("Numero no valido, Ingrese el numero de threads que se ejecutaran: ");
-			numeroThreads =Integer.parseInt(scanner.nextLine());
-		}
+			numeroThreads = scanner.nextInt();
+		} while (numeroThreads != 1 && numeroThreads != 2);
 
+		scanner.nextLine();
 		scanner.close();
+		
+		//Generador de contrasenas que se le pasara por parametro al o los passwordCracker para que lo usen.
+		GeneradorContrasenas generadorContrasenas = new GeneradorContrasenas();
 
 		//Se crean y se corren los Threads.
 		for(int cantidadThreads = 0; cantidadThreads < numeroThreads; cantidadThreads++)
 		{
-			PasswordCracker thread = new PasswordCracker(contraseñaHash, sal, algoritmoEncriptacion);
-			thread.run();
+			PasswordCracker thread = new PasswordCracker(hashBytes, sal, algoritmoEncriptacion, generadorContrasenas);
+			thread.start();
 		}
-		
-		//Generador de contrasenas que se le pasara por parametro al o los passwordCracker para que lo usen.
-		GeneradorContrasenas prueba = new GeneradorContrasenas();
 
-		for(int i = 0; i < 14988807; i++)
-			System.out.println(prueba.generarSiguienteContrasena() + i);
 	}
 }
--- src/main/java/org/example/PasswordCracker.java
+++ src/main/java/org/example/PasswordCracker.java
@@ -1,33 +1,60 @@
 package org.example;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordCracker extends Thread 
 {
 
 	//Atributos
-	private String contrasenaHash;
+	private byte[] contrasenaHash;
 
 	private String sal;
 
 	/**
 	 * Algoritmo de hash que se uso en la contraseña. 1 para SHA256 y 2 para SHA512. 
 	 */
-	private int algoritmoHash;
+	private String algoritmoHash;
 	
 	private static boolean decifrado;
 
-	public PasswordCracker(String pContrasenaHash, String pSal , int pAlgoritmoHash)
+	private static GeneradorContrasenas generadorContrasenas;
+
+	public PasswordCracker(byte[] pContrasenaHash, String pSal , String pAlgoritmoHash, GeneradorContrasenas pGeneradorContrasenas)
 	{
 		contrasenaHash = pContrasenaHash;
 		sal = pSal;
 		algoritmoHash = pAlgoritmoHash;
 		decifrado = false;
+		generadorContrasenas = pGeneradorContrasenas;
 	}
 	
 	public void run() 
 	{
-		
+		while(!decifrado)
+		{
+			//Se obtiene la siguiente contraseña a probar.
+			String contrasena = generadorContrasenas.generarSiguienteContrasena();
+			try {
+				byte[] hash = obtenerHash(contrasena, sal, algoritmoHash);
+				if(MessageDigest.isEqual(hash, contrasenaHash)){
+					decifrado = true;
+					System.out.println("La contraseña es: " + contrasena);
+				}else{
+					//System.out.println("La contraseña no es: " + contrasena);
+				}
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
+	public static byte[] obtenerHash(String contrasena, String sal, String algoritmo) throws NoSuchAlgorithmException{
+		MessageDigest md = MessageDigest.getInstance(algoritmo);
+		String concat = contrasena + sal;
+		md.update(concat.getBytes());
+		return md.digest();
+	}
 	
 	
 
